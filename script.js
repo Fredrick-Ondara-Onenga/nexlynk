ce067e82fab6f4f157c10a2990b77ac26ecd1888
Fredrick Ondara <bsee101j2023@students.tum.ac.ke>
2025-11-28 13:57:02 +0300

    update contact section script

--- script.js
+++ script.js
@@ -46,12 +46,7 @@
             });
         });
 
-        // Contact form submission
-        document.getElementById('contactForm').addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Thank you for your message! We will get back to you soon.');
-            this.reset();
-        });
+
 
         // Navbar scroll effect
         window.addEventListener('scroll', function() {
@@ -61,4 +56,5 @@
             } else {
                 header.classList.remove('scrolled');
             }
+
         });
